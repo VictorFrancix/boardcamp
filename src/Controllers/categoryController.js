4c496d56db897e0d0b9d028191d05224ba9d7d60
João Victor <jvictor.franci@gmail.com>
2022-05-30 13:47:48 -0300

    feat: add new endpoint POST /category

--- src/Controllers/categoryController.js
+++ src/Controllers/categoryController.js
@@ -7,3 +7,32 @@ export async function getCategories(req, res){
 
     res.status(200).send(result.rows);
 } 
+
+export async function sendCategory(req, res) {
+    try {
+        const result = await connection.query(
+            `SELECT * 
+            FROM categories 
+            WHERE name=$1`,
+            [req.body.name]
+        );
+
+        if (result.rows.length > 0) {
+            res.sendStatus(409);
+            return;
+        }
+
+        await connection.query(
+            `
+            INSERT INTO categories (name)
+            VALUES($1);
+        `,
+            [req.body.name]
+        );
+
+        res.sendStatus(201);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
--- src/routers/categoryRouters.js
+++ src/routers/categoryRouters.js
@@ -1,9 +1,15 @@
 import { Router} from "express";
 
-import { getCategories } from "../Controllers/categoryController.js";
+import { getCategories, sendCategory } from "../Controllers/categoryController.js";
+import { validateSchema } from "../middlewares/validateSchemaMiddleware.js";
+import { categorySchema } from "../schemas/categorySchema.js";
 
 const categoryRouters = Router();
 
 categoryRouters.get('/categories', getCategories);
+
+categoryRouters.post('/categories', (req,res,next) => {
+    validateSchema(req,res,next,categorySchema);},
+     sendCategory);
 
 export default categoryRouters; 
