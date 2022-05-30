c0d330d2ce10542b2af9d3f11ce859f9b332bbed
João Victor <jvictor.franci@gmail.com>
2022-05-30 16:48:40 -0300

    feat: add new endpoint POST /costumers

--- src/Controllers/customersController.js
+++ src/Controllers/customersController.js
@@ -31,10 +31,46 @@ export async function getCustomer(req, res){
             [id]
         );
 
+        if(result.rows.length === 0){
+            res.sendStatus(404);
+            return;
+        }
+
         res.status(200).send(result.rows);
     } catch(e){
         console.log(e);
         res.sendStatus(500);
         return;
+    }
+}
+
+export async function sendCustomer(req, res) {
+    const { name, phone, cpf, birthday } = req.body;
+
+    try {
+        const customer = await connection.query(
+            `SELECT * FROM customers
+            WHERE cpf = $1
+            `,
+            [cpf]
+        );
+
+        if (customer.rows.length > 0) {
+            res.sendStatus(409);
+            return;
+        }
+
+        await connection.query(
+            `INSERT INTO customers 
+            (name, phone, cpf, birthday)
+            VALUES ($1, $2, $3, $4)
+            `,
+            [name, phone, cpf, birthday]
+        );
+
+        res.sendStatus(201);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
     }
 }
--- src/routers/customersRouter.js
+++ src/routers/customersRouter.js
@@ -1,10 +1,18 @@
 import {Router} from "express";
 
-import {getCustomers, getCustomer} from "../Controllers/customersController.js";
+import {getCustomers, getCustomer, sendCustomer} from "../Controllers/customersController.js";
+import {validateSchema} from "../middlewares/validateSchemaMiddleware.js";
+import { customerSchema } from "../schemas/customerSchema.js";
+
 
 const customersRouter = Router();
 
 customersRouter.get('/customers', getCustomers);
+
 customersRouter.get('/customers/:id', getCustomer);
+
+customersRouter.post('/customers', (req, res, next) => {
+    validateSchema(req, res, next, customerSchema);
+}, sendCustomer);
 
 export default customersRouter;
